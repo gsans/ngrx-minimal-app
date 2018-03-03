9f018076773fd3c52de4ca082e24c7c40d9787ba
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2018-03-02 23:43:01 -0500

    Subscribe Loading component to the State

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromStore from '../reducers/index';
+import { SpinnerShow } from '../actions/spinner.actions';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-loading',
@@ -8,10 +10,14 @@ import * as fromStore from '../reducers/index';
   styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
+  loading: Observable<boolean>;
 
-  constructor(private store: Store<fromStore.State>) { }
+  constructor(private store: Store<fromStore.State>) { 
+    this.store.dispatch(new SpinnerShow());
+  }
 
   ngOnInit() {
+    this.loading = this.store.select('spinner');
   }
 
 }

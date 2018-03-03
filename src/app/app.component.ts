9178a1d03909a868be1b7e914a49aa86589d3c2c
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2018-03-03 02:55:47 -0500

    Add components inputs to include target

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as fromStore from './reducers/index';
+import { SpinnerShow } from './actions/spinner.actions';
 
 @Component({
   selector: 'app-root',
-  template: `<app-loading></app-loading>`,
+  template: `
+    <app-loading target="spinner-top" color="red"></app-loading>
+    <app-loading target="spinner-bottom" color="black"></app-loading>
+  `,
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private store: Store<fromStore.State>) { }
+
+  ngOnInit() {
+    this.store.dispatch(new SpinnerShow({ target: "spinner-top" }));
+    this.store.dispatch(new SpinnerShow({ target: "spinner-bottom" }));
+  }
 }
